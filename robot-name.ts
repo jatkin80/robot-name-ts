88c502486dda961316453f7c5c06a54edb03ec24
Julie <jatkin80@ford.com>
2022-05-10 16:00:39 -0500

    update for solution

--- robot-name.ts
+++ robot-name.ts
@@ -1,1 +1,48 @@
-export class Robot {}
+export class Robot {
+  name: string;
+  static usedNames = new Set<string>();
+  static releaseNames() {
+    this.usedNames.clear();
+  }
+
+  constructor() {
+    this.name = this.uniqueRandomName;
+  }
+
+  resetName() {
+    this.name = this.uniqueRandomName;
+  }
+
+  private get uniqueRandomName() {
+    let newName;
+    do {
+      newName = this.randomName;
+    } while (Robot.usedNames.has(newName));
+    Robot.usedNames.add(newName);
+    return newName;
+  }
+
+  private get randomName() {
+    return `${this.twoRandomLetters}${this.threeRandomDigits}`;
+  }
+
+  private get twoRandomLetters() {
+    const letter1 = this.randomLetter;
+    const letter2 = this.randomLetter;
+
+    return `${letter1}${letter2}`;
+  }
+
+  private get randomLetter() {
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const randomNumber = Math.floor(Math.random() * 26);
+    return letters[randomNumber];
+  }
+  private get threeRandomDigits() {
+    return `${this.randomNumber0to9}${this.randomNumber0to9}${this.randomNumber0to9}`;
+  }
+
+  private get randomNumber0to9() {
+    return Math.floor(Math.random() * 10);
+  }
+}
